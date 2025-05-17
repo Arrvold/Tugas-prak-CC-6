07af38cd6e0a98ef28296ebfb0d49d17629ff431
Arjikusna M <arjiduba@gmail.com>
2025-05-18 00:06:51 +0700

    edit db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-// Nyambungin db ke BE
+// Nyambungin db
 const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
   host: "35.225.144.134",
   dialect: "mysql",
